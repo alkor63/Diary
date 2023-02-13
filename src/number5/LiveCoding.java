08ca860c83431aa4bb675a10c603a8c557a8ba60
alkor63 <alkor@medi-spa.ru>
2023-02-14 01:37:48 +0300

    7 commit "причесал" всех потомков Task (убрал id & dataTime) LiveCoding к билету 5 см. в пакете Number5

---
+++ src/number5/LiveCoding.java
@@ -0,0 +1,38 @@
+package number5;
+
+import java.util.Scanner;
+
+public class LiveCoding {
+    public static void main(String[] args) {
+        double num = 1.0;
+        Scanner in = new Scanner(System.in);
+        System.out.println("При вводе double в качестве разделителя используйте запятую");
+        while (num != 0) {
+            System.out.print("Введите положительное число - сторону квадрата: ");
+            num = in.nextDouble();
+            if (num == 0) break;
+            try {
+                checkPositive(num);
+                System.out.println(" площадь квадрата со стороной " + num + " = " + square(num));
+            } catch (IllegalArgumentException e) {
+                System.out.println("вы ввели " + num + e.getMessage());
+            } finally {
+                System.out.println("(для выхода из программы введите '0')");
+            }
+        }
+        in.close();
+    }
+
+    public static double square(double x) {
+        double y;
+        y = x * x;
+        return y;
+    } // вычислдили квадрат аргумента
+
+    public static void checkPositive(double d) throws IllegalArgumentException {
+        if (d < 0.0) {
+            throw new IllegalArgumentException(" - сторона квадрата не может быть отрицательной !");
+        }
+    }
+
+}
